24b4d27d697870faa0feb523b680b896051bc0c0
imsiddis <axelaarre@gmail.com>
2024-04-12 06:50:14 +0100

    Added disclaimer to the startup.

--- main.py
+++ main.py
@@ -18,6 +18,32 @@ from internal_library.asset_functions import splash_screen, splash_logo_no_inden
 # This will be the main menu for the project.
 # It will be a numerical menu system.
 
+disclaimer_text = f'''
+## Disclaimer ##
+Disclaimer and Ethical Use Policy for The Rapid Rabbit Toolkit
+
+Purpose and Intended Use
+The Rapid Rabbit Toolkit is developed with the intent to support ethical hacking and cybersecurity defense activities. It is designed for educational purposes, security professionals, and technology enthusiasts to conduct responsible security assessments. Users of this toolkit are reminded to operate within all applicable legal and ethical boundaries and to ensure that its use aligns with the principles of constructive cybersecurity engagement.
+
+Responsible Disclosure
+Users who identify vulnerabilities with the aid of this toolkit are encouraged to engage in responsible disclosure practices. This includes notifying affected entities discreetly to allow for the rectification of vulnerabilities before any public disclosure, in accordance with established guidelines for responsible disclosure.
+
+Prohibition of Malicious Use
+The use of this toolkit for illegal or malicious activities is strictly prohibited. This includes, but is not limited to, deploying capabilities such as reverse shells or hash crackers without explicit authorization from the relevant authority. Engaging in unauthorized or harmful activities using this toolkit will lead to legal and ethical repercussions.
+
+Ethical Awareness and Education
+This toolkit includes resources designed to enhance the user’s understanding of cybersecurity ethics and legal compliance. Users are urged to secure necessary authorizations and understand the ethical implications before conducting any form of security assessments. We aim to promote an ethical hacking culture that respects privacy and the integrity of systems.
+
+Community Involvement
+Feedback and contributions from the cybersecurity community are highly valued. Users are invited to share their insights, suggestions, and concerns regarding both the ethical and security aspects of this toolkit to foster continuous improvement and ethical accountability in its development and use.
+
+Commitment to Ethical Integrity
+The development of The Rapid Rabbit Toolkit adheres to high standards of security and ethical integrity. Our goal is to provide a tool that not only advances cybersecurity techniques but also upholds the ethical principles that protect and enhance the digital landscape.
+
+By using The Rapid Rabbit Toolkit, you acknowledge and agree to these terms, committing to uphold the highest standards of ethical conduct in your cybersecurity endeavors.
+
+# End of Disclaimer #
+    '''
 
 info_box = f"This is the 'Rapid Rabbit Toolkit'.\n\nA lightweight toolkit for Cyber Professionals with the desire for a fast, easy to use set of tools in their profession.\nOne of the benefits of this toolkit is that it is easy to add your own tools or 'modules' as we call it, and the Rapid Rabbit will seamlessly integrate it to the toolkit.\nWe hope you will like it.\n\nBest Regards,\nSiddis"
 
@@ -31,7 +57,10 @@ def main():
     try:
         # This will display the splash screen for the project.
         clear_screen()
-
+        disclaimer = center_block_text(disclaimer_text)
+        print(disclaimer_text)
+        time.sleep(2)
+        clear_screen()
         print(splash_logo)
         print(splash_title)
         #input("\n\n\t\t\t\t\t\t\tPress 'Enter' to continue... ")
